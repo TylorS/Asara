94b6f9fc71938cd076a258b3d911618360de3564
Tylor Steinberger <tlsteinberger167@gmail.com>
2024-09-22 17:33:47 -0400

    WIP: cleanup lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,26 +4,12 @@ mod lexing;
 mod parsing;
 mod types;
 
-use crate::codegen::wasm::WasmCompiler;
-use crate::lexing::token::Token;
-use crate::parsing::ast::Program;
-use crate::types::TypeEnv;
-use logos::Logos;
+use codegen::wasm::WasmCompiler;
+use parsing::parser::parse;
 
 pub fn compile(source: &str) -> Result<Vec<u8>, String> {
-    let tokens = Token::lexer(source).collect::<Vec<_>>();
-    let ast = parse_tokens(tokens)?;
-    let type_env = type_check(&ast)?;
+    let ast = parse(source)?;
     let wasm = WasmCompiler::new().compile(&ast);
     Ok(wasm)
 }
 
-fn parse_tokens(tokens: Vec<Token>) -> Result<Program, String> {
-    // Implement parser
-    unimplemented!("Parser not yet implemented")
-}
-
-fn type_check(ast: &Program) -> Result<TypeEnv, String> {
-    // Implement type checker
-    unimplemented!("Type checker not yet implemented")
-}
